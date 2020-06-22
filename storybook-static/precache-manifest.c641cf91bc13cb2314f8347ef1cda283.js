7bb584e784e357e5d14cc4eaf34a1574e7473757
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-06-22 23:28:59 +0530

    - 10 Build storybook for deployment

---
+++ storybook-static/precache-manifest.c641cf91bc13cb2314f8347ef1cda283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27db64a80d50a56f0071c4e86bbc18f2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e5214b262dbf3f5acd13.bundle.js"
+  },
+  {
+    "url": "runtime~main.e5214b262dbf3f5acd13.bundle.js"
+  },
+  {
+    "revision": "d43e192e5687534fde0e16cc84f76dcc",
+    "url": "static/media/signUp.d43e192e.svg"
+  },
+  {
+    "url": "vendors~main.e5214b262dbf3f5acd13.bundle.js"
+  },
+  {
+    "url": "vendors~main.e5214b262dbf3f5acd13.bundle.js.LICENSE.txt"
+  }
+]);
